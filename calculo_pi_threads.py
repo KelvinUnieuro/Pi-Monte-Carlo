8004bc1094abcf4e60c630edce8771d09f28d701
Kelvin Lima <kelvinl65unieuro@gmail.com>
2024-05-06 21:22:31 -0300

    Adicionado documento e apresentação

--- calculo_pi_threads.py
+++ calculo_pi_threads.py
@@ -6,168 +6,175 @@ import concurrent.futures
 import random
 import time 
 
-class ValueProcess(multiprocessing.Process):  
-    def __init__(self, num_points, points_inside_circle, result_queue):
+
+class ProcessoValor(multiprocessing.Process):  
+    def __init__(self, num_pontos, pontos_dentro_circulo, fila_resultados):
         super().__init__()
-        self.num_points = num_points
-        self.points_inside_circle = points_inside_circle  
-        self.result_queue = result_queue  
+        self.num_pontos = num_pontos
+        self.pontos_dentro_circulo = pontos_dentro_circulo  
+        self.fila_resultados = fila_resultados  
 
+    # FUNÇÃO EXECUTAR
     def run(self):
-        points_inside_circle = monte_carlo_pi_part(self.num_points, self.points_inside_circle)
-        self.result_queue.put(points_inside_circle)
+        pontos_dentro_circulo = parte_pi_monte_carlo(self.num_pontos, self.pontos_dentro_circulo)
+        self.fila_resultados.put(pontos_dentro_circulo)
 
-def monte_carlo_pi_part_wrapper(num_points):
-    points_inside = 0
-    for _ in range(num_points):
+# FUNÇÃO AUXILIAR PARA CALCULAR PONTOS DENTRO DO CÍRCULO
+def parte_wrapper_pi_monte_carlo(num_pontos):
+    pontos_dentro = 0
+    for _ in range(num_pontos):
         x = random.uniform(-1, 1)
         y = random.uniform(-1, 1)
-        distance_to_origin = (x ** 2 + y ** 2) ** 0.5
-        if distance_to_origin <= 1:
-            points_inside += 1
-    return points_inside
+        distancia_origem = (x ** 2 + y ** 2) ** 0.5
+        if distancia_origem <= 1:
+            pontos_dentro += 1
+    return pontos_dentro
 
-def monte_carlo_pi_part(num_points, points_inside_circle):
-    points_inside = 0
-    for _ in range(num_points):
+#FUNÇÃO PARA CALCULAR PONTOS DENTRO DO CÍRCULO
+def parte_pi_monte_carlo(num_pontos, pontos_dentro_circulo):
+    pontos_dentro = 0
+    for _ in range(num_pontos):
         x = random.uniform(-1, 1)
         y = random.uniform(-1, 1)
-        distance_to_origin = (x ** 2 + y ** 2) ** 0.5
-        if distance_to_origin <= 1:
-            points_inside += 1
-    with points_inside_circle.get_lock():
-        points_inside_circle.value += points_inside
-    return points_inside
+        distancia_origem = (x ** 2 + y ** 2) ** 0.5
+        if distancia_origem <= 1:
+            pontos_dentro += 1
+    with pontos_dentro_circulo.get_lock():
+        pontos_dentro_circulo.value += pontos_dentro
+    return pontos_dentro
 
-def monte_carlo_pi_concurrent(num_points, num_workers):  
-    start_time = time.time()
-    with concurrent.futures.ProcessPoolExecutor(max_workers=num_workers) as executor:
-        total_points_inside_circle = sum(executor.map(monte_carlo_pi_part_wrapper, [num_points // num_workers] * num_workers))
+# CALCULA O PI USANDO concurrent.futures
+def monte_carlo_pi_concurrent(num_pontos, num_trabalhadores):  
+    tempo_inicio = time.time()
+    with concurrent.futures.ProcessPoolExecutor(max_workers=num_trabalhadores) as executor:
+        total_pontos_dentro_circulo = sum(executor.map(parte_wrapper_pi_monte_carlo, [num_pontos // num_trabalhadores] * num_trabalhadores))
 
-    pi_approximation = 4 * total_points_inside_circle / num_points
-    end_time = time.time()
-    return pi_approximation, end_time - start_time
+    aproximacao_pi = 4 * total_pontos_dentro_circulo / num_pontos
+    tempo_fim = time.time()
+    return aproximacao_pi, tempo_fim - tempo_inicio
 
-def monte_carlo_pi_threads(num_points, num_threads):  
-    start_time = time.time()
-    points_inside_circle = multiprocessing.Value('i', 0)
+# CALCULA O PI USANDO THREADS
+def monte_carlo_pi_threads(num_pontos, num_threads):  
+    tempo_inicio = time.time()
+    pontos_dentro_circulo = multiprocessing.Value('i', 0)
     threads = []
-    points_per_thread = num_points // num_threads
-    semaphore = threading.Semaphore(num_threads) 
+    pontos_por_thread = num_pontos // num_threads
+    semaforo = threading.Semaphore(num_threads) 
     for _ in range(num_threads):
-        thread = threading.Thread(target=monte_carlo_pi_part_with_semaphore, args=(points_per_thread, points_inside_circle, semaphore))
+        thread = threading.Thread(target=parte_pi_monte_carlo_com_semaforo, args=(pontos_por_thread, pontos_dentro_circulo, semaforo))
         threads.append(thread)
         thread.start()
     for thread in threads:
         thread.join()
-    total_points_inside_circle = points_inside_circle.value
-    pi_approximation = 4 * total_points_inside_circle / num_points
-    end_time = time.time()
-    return pi_approximation, end_time - start_time
+    total_pontos_dentro_circulo = pontos_dentro_circulo.value
+    aproximacao_pi = 4 * total_pontos_dentro_circulo / num_pontos
+    tempo_fim = time.time()
+    return aproximacao_pi, tempo_fim - tempo_inicio
 
-def monte_carlo_pi_part_with_semaphore(num_points, points_inside_circle, semaphore):  
-    semaphore.acquire()
+# FUNÇÃO PARA CALCULAR PONTOS DENTRO DO CÍRCULO COM SEMÁFORO
+def parte_pi_monte_carlo_com_semaforo(num_pontos, pontos_dentro_circulo, semaforo):  
+    semaforo.acquire()
     try:
-        points_inside = 0
-        for _ in range(num_points):
+        pontos_dentro = 0
+        for _ in range(num_pontos):
             x = random.uniform(-1, 1)
             y = random.uniform(-1, 1)
-            distance_to_origin = (x ** 2 + y ** 2) ** 0.5
-            if distance_to_origin <= 1:
-                points_inside += 1
-        with points_inside_circle.get_lock():
-            points_inside_circle.value += points_inside
+            distancia_origem = (x ** 2 + y ** 2) ** 0.5
+            if distancia_origem <= 1:
+                pontos_dentro += 1
+        with pontos_dentro_circulo.get_lock():
+            pontos_dentro_circulo.value += pontos_dentro
     finally:
-        semaphore.release()
+        semaforo.release()
 
-def monte_carlo_pi_multiprocessing(num_points, num_processes):
-    start_time = time.time()
-    points_inside_circle = multiprocessing.Value('i', 0)
-    processes = []
-    points_per_process = num_points // num_processes
-    for _ in range(num_processes):
-        process = multiprocessing.Process(target=monte_carlo_pi_part, args=(points_per_process, points_inside_circle))
-        processes.append(process)
-        process.start()
-    for process in processes:
-        process.join()
-    total_points_inside_circle = points_inside_circle.value
-    pi_approximation = 4 * total_points_inside_circle / num_points
-    end_time = time.time()
-    return pi_approximation, end_time - start_time
+#CALCULA PI USANDO multiprocessing
+def monte_carlo_pi_multiprocessing(num_pontos, num_processos):
+    tempo_inicio = time.time()
+    pontos_dentro_circulo = multiprocessing.Value('i', 0)
+    processos = []
+    pontos_por_processo = num_pontos // num_processos
+    for _ in range(num_processos):
+        processo = multiprocessing.Process(target=parte_pi_monte_carlo, args=(pontos_por_processo, pontos_dentro_circulo))
+        processos.append(processo)
+        processo.start()
+    for processo in processos:
+        processo.join()
+    total_pontos_dentro_circulo = pontos_dentro_circulo.value
+    aproximacao_pi = 4 * total_pontos_dentro_circulo / num_pontos
+    tempo_fim = time.time()
+    return aproximacao_pi, tempo_fim - tempo_inicio
 
-def monte_carlo_pi_threads_with_semaphore(num_points, num_threads):  
-    start_time = time.time()
-    points_inside_circle = multiprocessing.Value('i', 0)
+# CALCULA PI USANDO THREADS E SEMÁFOROS
+def monte_carlo_pi_threads_com_semaforo(num_pontos, num_threads):  
+    tempo_inicio = time.time()
+    pontos_dentro_circulo = multiprocessing.Value('i', 0)
     threads = []
-    points_per_thread = num_points // num_threads
-    semaphore = threading.Semaphore(num_threads)
+    pontos_por_thread = num_pontos // num_threads
+    semaforo = threading.Semaphore(num_threads)
     for _ in range(num_threads):
-        thread = threading.Thread(target=monte_carlo_pi_part_with_semaphore, args=(points_per_thread, points_inside_circle, semaphore))
+        thread = threading.Thread(target=parte_pi_monte_carlo_com_semaforo, args=(pontos_por_thread, pontos_dentro_circulo, semaforo))
         threads.append(thread)
         thread.start()
     for thread in threads:
         thread.join()
-    total_points_inside_circle = points_inside_circle.value
-    pi_approximation = 4 * total_points_inside_circle / num_points
-    end_time = time.time()
-    return pi_approximation, end_time - start_time
+    total_pontos_dentro_circulo = pontos_dentro_circulo.value
+    aproximacao_pi = 4 * total_pontos_dentro_circulo / num_pontos
+    tempo_fim = time.time()
+    return aproximacao_pi, tempo_fim - tempo_inicio
 
-def calculate_pi(method, num_points, num_workers):
-    if method == "Threads":
-        return monte_carlo_pi_threads(num_points, num_workers)
-    elif method == "Multiprocessamento":
-        return monte_carlo_pi_multiprocessing(num_points, num_workers)
-    elif method == "Concurrent.Futures":
-        return monte_carlo_pi_concurrent(num_points, num_workers)
-    elif method == "Threads com Semáforo":
-        return monte_carlo_pi_threads_with_semaphore(num_points, num_workers)
+# SELECIONA A FUNÇÃO DE CÁLCULO DE PI COM BASE NO MÉTODO ESCOLHIDO
+def calcular_pi(metodo, num_pontos, num_trabalhadores):
+    if metodo == "Threads":
+        return monte_carlo_pi_threads(num_pontos, num_trabalhadores)
+    elif metodo == "Multiprocessamento":
+        return monte_carlo_pi_multiprocessing(num_pontos, num_trabalhadores)
+    elif metodo == "Concurrent.Futures":
+        return monte_carlo_pi_concurrent(num_pontos, num_trabalhadores)
+    elif metodo == "Threads com Semáforo":
+        return monte_carlo_pi_threads_com_semaforo(num_pontos, num_trabalhadores)
 
-def on_button_click(method, num_workers_entry): 
+# FUNÇÃO CHAMADA QUANDO O BOTÃO É CLICADO
+def ao_clicar_botao(metodo, entrada_num_trabalhadores): 
     try:
-        num_workers = int(num_workers_entry.get())
-        pi_value, process_time = calculate_pi(method, 1000000, num_workers)
-        messagebox.showinfo("Resultado", f"Valor aproximado de Pi ({method}): {pi_value}\nTempo de processamento: {process_time} segundos")
+        num_trabalhadores = int(entrada_num_trabalhadores.get())  
+        valor_pi, tempo_processo = calcular_pi(metodo, 1000000, num_trabalhadores)
+        messagebox.showinfo("Resultado", f"Valor aproximado de Pi ({metodo}): {valor_pi}\nTempo de processamento: {tempo_processo} segundos")  
     except ValueError:
-        messagebox.showerror("Erro", "Insira um número válido de workers.")
+        messagebox.showerror("Erro", "Insira um número válido de workers.")  
     except Exception as e:
-        messagebox.showerror("Erro", f"Ocorreu um erro: {str(e)}")
+        messagebox.showerror("Erro", f"Ocorreu um erro: {str(e)}")  
 
+# CRIA A INTERFACE GRÁFICA
+def criar_gui(): 
+    raiz = tk.Tk() 
+    raiz.title("Cálculo de Pi") 
+    raiz.geometry("600x250") 
 
-def create_gui(): 
-    def back_to_menu():  # Função para voltar ao menu principal
-        root.destroy()  # Fecha a janela atual
-        create_gui()    # Chama a função create_gui() novamente para voltar ao menu principal
+    # FUNÇÃO CHAMADA QUANDO UM MÉTODO É SELECIONADO
+    def ao_selecionar_metodo(metodo):  
+        frame_metodo.destroy()
+        rotulo_num_trabalhadores = tk.Label(raiz, text=f"Insira o número de {metodo.lower()}:")  
+        rotulo_num_trabalhadores.pack()
+        entrada_num_trabalhadores = tk.Entry(raiz)
+        entrada_num_trabalhadores.pack()
 
-    root = tk.Tk() 
-    root.title("Cálculo de Pi") 
-    root.geometry("600x250") 
+        def ao_confirmar():  
+            ao_clicar_botao(metodo, entrada_num_trabalhadores)
 
-    method_frame = tk.Frame(root)  
-    method_frame.pack(pady=20)
+        botao_confirmar = tk.Button(raiz, text="Confirmar", command=ao_confirmar)  
+        botao_confirmar.pack()
 
-    for method in ["Threads", "Multiprocessamento", "Concurrent.Futures", "Threads com Semáforo"]:  
-        method_button = tk.Button(method_frame, text=method, command=lambda m=method: on_method_selected(m))  
-        method_button.pack(side=tk.LEFT, padx=10)
-    
-    back_button = tk.Button(root, text="Voltar ao Menu", command=back_to_menu)  # Botão para voltar ao menu
-    back_button.pack(side=tk.BOTTOM, pady=10)
+        # Botão para voltar ao menu
+        botao_voltar_menu = tk.Button(raiz, text="Voltar ao Menu", command=criar_gui)  
+        botao_voltar_menu.pack()
 
-    def on_method_selected(method):  
-        method_frame.destroy()
-        num_workers_label = tk.Label(root, text=f"Insira o número de {method.lower()}:")  
-        num_workers_label.pack()
-        num_workers_entry = tk.Entry(root)
-        num_workers_entry.pack()
+    frame_metodo = tk.Frame(raiz)  
+    frame_metodo.pack(pady=20)
 
-        def on_confirm():  
-            on_button_click(method, num_workers_entry)
-
-        confirm_button = tk.Button(root, text="Confirmar", command=on_confirm)  
-        confirm_button.pack()
-
-    root.mainloop()
-
+    for metodo in ["Threads", "Multiprocessamento", "Concurrent.Futures", "Threads com Semáforo"]:  
+        botao_metodo = tk.Button(frame_metodo, text=metodo, command=lambda m=metodo: ao_selecionar_metodo(m))  
+        botao_metodo.pack(side=tk.LEFT, padx=10)
+    raiz.mainloop()
 
 if __name__ == "__main__":
-    create_gui()
+    criar_gui()
